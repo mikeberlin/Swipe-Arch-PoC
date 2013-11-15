9d37687e4fb021bfe008e696d46d8d1ed83367f8
Mike Berlin <mikeberlin@me.com>
2013-11-15 14:38:08 -0600

    Removed Actions and used physics body and world properties to simulate the movement desired.

--- iOS/Scenes/GameScene.cs
+++ iOS/Scenes/GameScene.cs
@@ -16,7 +16,11 @@ namespace iOS
 		private const string SPRITE_NAME = "Sprite-To-Swipe";
 
 		private SKSpriteNode Background { get; set; }
+		private SKSpriteNode Ground { get; set; }
 		private SKSpriteNode SelectedNode { get; set; }
+
+		private PointF StartSwipePosition { get; set; }
+		private DateTime StartSwipeTime { get; set; }
 
 		#endregion
 
@@ -24,27 +28,43 @@ namespace iOS
 
 		public GameScene (SizeF size)
 		{
-			this.Size = size;
-
 			// Load the background
 			this.Background = new SKSpriteNode ("blue-shooting-stars-vertical");
 			this.Background.Name = "background";
 			this.Background.AnchorPoint = new PointF (0, 0);
 			this.AddChild (Background);
 
-			// Loading the images and adding to scene
-			List<string> imageNames = new List<string>() {"bird", "cat", "dog", "turtle"};
+			this.Size = size;
+			this.PhysicsWorld.Gravity = new CGVector (this.PhysicsWorld.Gravity.dx, -3.8f);
+			this.PhysicsBody = SKPhysicsBody.BodyWithEdgeLoopFromRect (this.Background.Frame);
 
-			int i = 0;
-			foreach (string imageName in imageNames) {
-				float offsetFraction = ((float)(i++ + 1) / (imageNames.Count + 1));
+			// Setup the ground node
+			this.Ground = new SKSpriteNode (UIColor.Brown, new SizeF (this.Frame.Width, 20f));
+			this.Ground.Position = new PointF (this.Frame.Width / 2, 0);
+			this.Ground.PhysicsBody = SKPhysicsBody.BodyWithRectangleOfSize (this.Ground.Size);
+			this.Ground.PhysicsBody.Dynamic = false;
+			this.Ground.PhysicsBody.AffectedByGravity = false;
+			this.Background.AddChild (this.Ground);
 
-				SKSpriteNode sprite = new SKSpriteNode (imageName);
-				sprite.Name = SPRITE_NAME;
-				sprite.Position = new PointF (size.Width * offsetFraction, 50f);
+			SKSpriteNode sprite = new SKSpriteNode ("dog");
+			sprite.Name = SPRITE_NAME;
+			sprite.Position = new PointF (35f, 50f);
+			sprite.PhysicsBody = SKPhysicsBody.BodyWithRectangleOfSize (sprite.Size);
+			sprite.PhysicsBody.Dynamic = true;
+			sprite.PhysicsBody.AffectedByGravity = true;
+			sprite.PhysicsBody.AllowsRotation = true;
+			sprite.PhysicsBody.Mass = 0.4f;
+			this.Background.AddChild (sprite);
 
-				this.Background.AddChild (sprite);
-			}
+			SKSpriteNode applyLeftImpulse = new SKSpriteNode (UIColor.Orange, new SizeF (50f, 50f));
+			applyLeftImpulse.Position = new PointF (50f, 250f);
+			applyLeftImpulse.Name = "APPLY-LEFT-IMPULSE";
+			this.AddChild (applyLeftImpulse);
+
+			SKSpriteNode applyRightImpulse = new SKSpriteNode (UIColor.Orange, new SizeF (50f, 50f));
+			applyRightImpulse.Position = new PointF (270f, 250f);
+			applyRightImpulse.Name = "APPLY-RIGHT-IMPULSE";
+			this.AddChild (applyRightImpulse);
 		}
 
 		#endregion
@@ -55,8 +75,11 @@ namespace iOS
 		{
 			base.DidMoveToView (view);
 
-			UIPanGestureRecognizer gestureRecognizer = new UIPanGestureRecognizer (HandlePanFrom);
-			this.View.AddGestureRecognizer (gestureRecognizer);
+			UIPanGestureRecognizer panGesture = new UIPanGestureRecognizer (HandlePanFrom);
+			UITapGestureRecognizer tapGesture = new UITapGestureRecognizer (HandleTapFrom);
+
+			this.View.AddGestureRecognizer (panGesture);
+			this.View.AddGestureRecognizer (tapGesture);
 		}
 
 		public override void Update (double currentTime)
@@ -64,10 +87,10 @@ namespace iOS
 			base.Update (currentTime);
 
 			if ((this.SelectedNode != null) && (this.SelectedNode.Name == SPRITE_NAME)) {
-				if (this.SelectedNode.Position.Y >= 300) {
-					this.Background.Position = BoundLayerPosition (new PointF (this.Background.Position.X, this.Background.Position.Y - 20));
-				} else if (this.SelectedNode.Position.Y + this.Background.Position.Y <= 100) {
-					this.Background.Position = BoundLayerPosition (new PointF (this.Background.Position.X, this.Background.Position.Y + 20));
+				if (this.SelectedNode.Position.Y >= 500) {
+					this.Background.Position = BoundLayerPosition (new PointF (this.Background.Position.X, this.Background.Position.Y - 80));
+				} else if (this.SelectedNode.Position.Y + this.Background.Position.Y <= 300) {
+					this.Background.Position = BoundLayerPosition (new PointF (this.Background.Position.X, this.Background.Position.Y + 80));
 				}
 			}
 		}
@@ -76,80 +99,52 @@ namespace iOS
 
 		#region Private Methods
 
-		private void PanForTranslation(PointF translation)
+		private void HandlePanFrom(UIPanGestureRecognizer recognizer)
 		{
-			PointF translatedPoint = new PointF (this.SelectedNode.Position.X + translation.X,
-			                                     this.SelectedNode.Position.Y + translation.Y);
+			if (recognizer.State == UIGestureRecognizerState.Began) {
+				PointF currentLocation = recognizer.LocationInView (recognizer.View);
+				currentLocation = this.ConvertPointFromView (currentLocation);
 
-			if (this.SelectedNode.Name == SPRITE_NAME) {
-				this.SelectedNode.Position = translatedPoint;
+				this.SelectedNode = (SKSpriteNode)this.GetNodeAtPoint (currentLocation);
 			}
-			else {
-				PointF boundedPoint = translatedPoint;
-				boundedPoint.X = this.Position.X;
-				boundedPoint.Y = Math.Max (Math.Min (boundedPoint.Y, 0), (this.Size.Height - this.Background.Size.Height));
-				this.Background.Position = boundedPoint;
+			else if (recognizer.State == UIGestureRecognizerState.Changed || this.SelectedNode.Name != SPRITE_NAME) {
+				PointF translation = recognizer.TranslationInView (recognizer.View);
+				translation = new PointF (translation.X, -translation.Y);
+
+				this.Background.Position = this.BoundLayerPosition(new PointF (this.SelectedNode.Position.X + translation.X, this.SelectedNode.Position.Y + translation.Y));
+			}
+			else if (recognizer.State == UIGestureRecognizerState.Ended) {
+				if (this.SelectedNode.Name == SPRITE_NAME) {
+					PointF velocity = recognizer.VelocityInView (recognizer.View);
+					velocity = new PointF (velocity.X, -velocity.Y);
+
+					this.SelectedNode.PhysicsBody.ApplyImpulse (new CGVector (velocity.X, velocity.Y));
+				}
+			}
+		}
+
+		private void HandleTapFrom(UITapGestureRecognizer recognizer)
+		{
+			PointF currentLocation = recognizer.LocationInView (recognizer.View);
+			currentLocation = this.ConvertPointFromView (currentLocation);
+
+			SKSpriteNode tappedNode = (SKSpriteNode)this.GetNodeAtPoint (currentLocation);
+
+			if (tappedNode.Name == "APPLY-LEFT-IMPULSE") {
+				this.SelectedNode.PhysicsBody.ApplyImpulse (new CGVector (-25f, 0));
+			} else if (tappedNode.Name == "APPLY-RIGHT-IMPULSE") {
+				this.SelectedNode.PhysicsBody.ApplyImpulse (new CGVector (25f, 0));
 			}
 		}
 
 		private PointF BoundLayerPosition(PointF boundedPoint)
 		{
-			SizeF windowSize = this.Size;
-
 			PointF returnValue = boundedPoint;
 			returnValue.X = this.Position.X;
 			returnValue.Y = Math.Min (returnValue.Y, 0);
-			returnValue.Y = Math.Max (returnValue.Y, windowSize.Height - this.Background.Size.Height);
+			returnValue.Y = Math.Max (returnValue.Y, this.Size.Height - this.Background.Size.Height);
 
 			return returnValue;
-		}
-
-		private void HandlePanFrom(UIPanGestureRecognizer recognizer)
-		{
-			PointF touchLocation = recognizer.LocationInView (recognizer.View);
-			touchLocation = this.ConvertPointFromView (touchLocation);
-
-			SKSpriteNode touchedNode = (SKSpriteNode)this.GetNodeAtPoint (touchLocation);
-
-			PointF translation = recognizer.TranslationInView (this.View);
-			translation = new PointF (translation.X, -translation.Y);
-
-			if (recognizer.State == UIGestureRecognizerState.Began) {
-				this.SelectedNode = touchedNode;
-			}
-			else if (recognizer.State == UIGestureRecognizerState.Changed) {
-				this.PanForTranslation (translation);
-				recognizer.SetTranslation (new PointF (0, 0), this.View);
-			}
-			else if (recognizer.State == UIGestureRecognizerState.Ended) {
-				if (this.SelectedNode.Name == SPRITE_NAME) {
-					PointF velocity = recognizer.VelocityInView (this.View);
-					velocity = new PointF (velocity.X, -velocity.Y);
-					float magnitude = (float)Math.Sqrt ((velocity.X * velocity.X) + (velocity.Y * velocity.Y));
-					float slideMult = (magnitude / 300);
-
-					// increase for more of a slide
-					float slideFactor = (float)(0.5 * slideMult);
-
-					PointF finalPoint = new PointF (this.SelectedNode.Position.X + (velocity.X * slideFactor),
-					                               this.SelectedNode.Position.Y + (velocity.Y * slideFactor));
-
-					finalPoint.X = Math.Min (Math.Max (finalPoint.X, 0), this.Background.Size.Width);
-					finalPoint.Y = Math.Min (Math.Max (finalPoint.Y, 0), this.Background.Size.Height);
-
-					this.SelectedNode.RemoveAllActions ();
-
-					// hoping that I can use the physics engine to assist with the arch
-					this.SelectedNode.PhysicsBody = SKPhysicsBody.BodyWithRectangleOfSize(this.SelectedNode.Size);
-					this.SelectedNode.PhysicsBody.Dynamic = true;
-
-					SKAction moveTo = SKAction.MoveTo (finalPoint, slideFactor);
-					moveTo.TimingMode = SKActionTimingMode.EaseOut;
-					this.SelectedNode.RunAction (moveTo, () => {
-						// TODO: Callback, but need to start curving at top of arch before this happens right?
-					});
-				}
-			}
 		}
 
 		#endregion
